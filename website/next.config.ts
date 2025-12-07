3fb4b8d6d03e364e53ae0c15f0a4164ad1746a0d
Tomas Morales <tomas.morales.ber@gmail.com>
2025-12-07 14:05:01 +0100

    Clean up repo for Vercel

--- website/next.config.ts
+++ website/next.config.ts
@@ -4,7 +4,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
